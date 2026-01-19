7bb7d342909b62238862b0ea89b40fd44e8c93ef
jzonthemtn <jeff.zemerick@philterd.ai>
2026-01-19 14:58:00 -0500

    Extracting medical conditions.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from gliner import GLiNER
+from transformers import pipeline
 from waitress import serve
 
 __version__ = "1.2.3"
@@ -13,6 +14,12 @@ model_name = os.getenv("MODEL_NAME", "philterd/ph-eye-pii-base")
 print("Using model " + model_name)
 model = GLiNER.from_pretrained(model_name)
 print("Model loaded and ready to serve requests")
+
+# Initialize the Medical NER pipeline
+print("Loading model...")
+medical_ner_pipeline = pipeline("token-classification", model="blaze999/Medical-NER", aggregation_strategy="simple")
+print("Model loaded successfully!")
+
 
 @app.route("/status", methods=["GET"])
 def status():
@@ -34,14 +41,37 @@ def find():
             labels = ["Person"]
 
         entities = model.predict_entities(text, labels, threshold=threshold)
+        medical_conditions = extract_medical_conditions(text)
 
-        return entities, 200
+        all_entities = entities + medical_conditions
+
+        return all_entities, 200
 
     except Exception as e:
         print(str(e))
         return str(e), 500
 
 
+def extract_medical_conditions(text):
+
+    results = medical_ner_pipeline(text)
+
+    entities = []
+    for res in results:
+
+        if res['entity_group'] == 'DISEASE_DISORDER':
+
+            entities.append({
+                "label": res['entity_group'],
+                "score": float(res['score']),
+                "text": res['word'],
+                "start": res['start'],
+                "end": res['end']
+            })
+
+
+    return entities
+
+
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=5000)
-
