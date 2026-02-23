bbe01181894e157456552f8ca1512c6548f359ea
Jeff <jeff.zemerick@philterd.ai>
2026-02-22 19:03:53 -0500

    Updating test script.

--- test.sh
+++ test.sh
@@ -2,15 +2,22 @@
 
 curl -s -X POST http://localhost:5000/find \
   -H "Content-Type: application/json" \
-  -d'{"text": "Bonjour, je m''appelle Sophie.", "threshold": 0.3, "labels": ["person"]}' | jq
+  -d'{"text": "He was in Buffalo Hospital and then ECMC Hospital.", "threshold": 0.3}' | jq
 
 # Expected output:
 #[
 #  {
-#    "end": 27,
-#    "label": "person",
-#    "score": 0.9863523244857788,
-#    "start": 22,
-#    "text": "Sophie"
+#    "end": 26,
+#    "label": "hospital",
+#    "score": 0.9917145371437073,
+#    "start": 11,
+#    "text": "Buffalo Hospital"
+#  },
+#  {
+#    "end": 49,
+#    "label": "hospital",
+#    "score": 0.9758248329162598,
+#    "start": 37,
+#    "text": "ECMC Hospital"
 #  }
 #]
